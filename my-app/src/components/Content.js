df22b1f628d5dfb6a440ed62e9caea1fc8ce6854
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-10-01 11:10:24 +0000

    Change content.js from class component to function component.

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,72 +1,55 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
 import Loader from "./Loader";
 
-export class Content extends Component {
-    constructor(props) {
-        super(props)
+function Content() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [fetchedPosts, setFetchedPosts] = useState([]);
 
-    this.state = {
-        isLoaded: false,
-        posts: savedPosts,
-        
-    }
-
-    this.inputSearch = React.createRef();
-
-    }
-
-    getData() {
+    useEffect(()=>{
         setTimeout(() => {
-            console.log(true)
-            this.setState({
-                isLoaded: true
-            })
+            setIsLoaded(true);
+            setFetchedPosts(savedPosts);
         }, 2000)
-    }
-
-    componentDidMount() {
-        this.getData()
-    }
-
-    handleChangeEvent = (event) => {
-        const inputSearch = event.target.value.toLowerCase();
-        console.log(inputSearch)
-        const filteredPosts = savedPosts.filter(post => 
-            {
-                return post.name.toLowerCase().includes(inputSearch)
-
-            })
-        this.setState ({
-           posts: filteredPosts
-        })
-    } 
+    }, []);
     
-    render() {
-        return (
-            <div className={css.Content}>
-                
-                <div className={css.TitleBar}>
-                    <div>
-                        <h1>My Photos</h1>
-                    </div>
-                    <div>
-                        <form>
-                            <label htmlFor="id-search">Search: </label>
-                            <input id="id-search" name="search" ref={this.inputSearch} onChange={(event) => this.handleChangeEvent(event)} type="text" placeholder="By Author" />
-                            <h4>Posts found: {this.state.posts.length} </h4>
-                        </form>
-                    </div>
-                </div>
-
-                <div className={css.SearchResults}>
-                    {this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader />}
-                </div>
-            </div>
-        )
+    const handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        
+        setFetchedPosts(filteredPosts)
     }
+
+    return (
+        <div className={css.Content}>
+            <div className={css.TitleBar}>
+                <h1>My Photos</h1>
+                <form>
+                    <label htmlFor="searchinput">Search</label>
+                    <input 
+                    type="search" 
+                    id="searchinput" 
+                    onChange={(e)=>{handleChange(e)}}
+                    />
+                    <h4>Posts Found: {fetchedPosts.length}</h4>
+                </form>
+                
+            </div>
+            <div className={css.SearchResults}>
+                {
+                    isLoaded ?
+                    <PostItem savedPosts={fetchedPosts} />
+                    : <Loader />
+
+                }  
+    
+            </div>
+        </div>
+    )
 }
 
 export default Content
